8ab9688e747496132296a89f6312e4bc697e6e01
Gaurav <sahu.gaurav719@gmail.com>
2016-04-04 01:54:38 +0530

    get the CG!
    Added the department

--- getCG_erp.py
+++ getCG_erp.py
@@ -12,20 +12,18 @@ url = 'https://erp.iitkgp.ernet.in/StudentPerformance/view_performance.jsp?rolln
 br.open(url)
 soup = BeautifulSoup(br.response().read().replace('&nbsp','').replace('<b>','<td>'),'lxml')
 p = soup.findAll('td')
-sem=[]
-cgpa=[]
 temp = 1
 
 for i in range(0,len(p)):
 	if(p[i].string==' CGPA'):
-		temp+=1
+		temp+=1 
 
-print("CGPA of %s is as follows:"%(p[6].string))
+print("CGPA of %s from the Department of %s is as follows:"%(p[6].string,p[9].string))
 for j in range(0,len(p)):
 	if(p[j].string==' CGPA'):
 		if(p[j+1].string==None):
 			print ''
 		else:
-			print("\n\t %s at the end of Semester %s"%(p[j+1].string,temp-1))
+			print("\n\t Semester %s --> %s"%(temp-1,p[j+1].string))
 		temp-=1
 # print ("%s"%(time.time()-start_time))
